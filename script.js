f93129259f4a0eb6f9074401fb56212dc45c2272
Aadarsh ojha <aadarshojha46@gmail.com>
2026-04-14 09:10:33 +0530

    basic commit

---
+++ script.js
@@ -0,0 +1,127 @@
+// let a = 10;
+// a = 20;
+// console.log(a);
+
+
+// const b = 20
+// b = 10;
+
+// console.log(b);
+
+
+// data types in js
+// 1. primitive data types
+// 2. non-primitive data types
+
+// primitive data types
+// 1. number    
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. symbol
+
+// let a = 10;
+// console.log(typeof a);
+// console.log(typeof 10);
+// console.log(typeof "10");
+// console.log(typeof true);
+
+
+// operators in js
+// 1. arithmetic operators
+// 2. assignment operators  
+// 3. comparison operators
+// 4. logical operators
+// 5. bitwise operators
+// 6. ternary operator      
+// arithmetic operators
+// let a = 10;
+// let b = 20;  
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// assignment operators
+// let a = 10;
+// a += 20; // a = a + 20
+// console.log(a);
+// comparison operators
+// let a = 10;      
+// console.log(a == 10);    
+// logical operators
+// let a = 10;
+// let b = 20;  
+// console.log(a > 5 && b < 30); // true
+// console.log(a > 15 || b < 30); // true
+// console.log(!(a > 15)); // true
+// ternary operator
+// let a = 10;
+// let result = a > 5 ? "Greater than 5" : "Less than or equal to 5";
+// console.log(result);
+
+
+// let a = 2;
+// let b = 7;
+// console.log(a ** b);
+
+
+// console.log(5 == "5"); // true (type coercion)
+// console.log(5 === "5");// false (strict equality, no type coercion)
+
+
+// conditions in js  
+
+
+// let num  = -3;
+// if(num>=0){
+//     console.log("The number is positive");
+// } else {
+//     console.log("The number is not positive");
+// }
+
+
+// let num  = 5;
+// if(num%2==0){
+//     console.log("The number is even");
+// } else {
+//     console.log("The number is odd");
+// }
+
+
+// loops in js
+// for loop
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+// while loop
+// let i = 0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+//    
+// let i = 0;
+// while(i<11){
+//     console.log(i);
+//     i++;
+    
+// }
+
+// let sum = 0;
+// for(let i =0;i<6;i++){
+//     sum = sum +i;
+
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
