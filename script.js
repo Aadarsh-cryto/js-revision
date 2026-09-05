53c147d366ec70e5b8e49976bb90382df7aca620
Aadarsh ojha <aadarshojha46@gmail.com>
2026-09-05 11:32:16 +0530

    Add files via upload
    objects in js , crud on objects, methods of objects in js

--- script.js
+++ script.js
@@ -1,113 +1,96 @@
-// !.from a sentence find the first word longer than 5 letters
-// const str = 'we are here for you sweetandsalt bilaspur';
-// const op = str.split(" ").find((el)=>{
-// return el.length>5;
-// })
-// console.log(op);
-
-//  2 find the first string that containes letter z
-
-// const arr = ["hello","zeb","hdid"];
-// let op = arr.find((el)=>{
-//     return el.includes("z");
-
-// })
-// console.log(op);
-
-// 3. get all words from a string that star with capital word
-
-// let str = ' javascrip Ghh Kjj';
-// let op = str.split(" ").filter((el)=>{
-//     return el.charAt(0).toUpperCase() == el.charAt(0);
-// })
-// console.log(op);
+// objects - in obj we store the data in key and value format 
+// create 
+let students = {
+    name : "addy",
+    age : 22,
+    course : "b.tech cse "
+};
 
 
-// 4. convert all words in an array to their first three letters
-// const arr = ['javascript','react','python','node'];
-// let op = arr.map((el)=>{
-// return el.slice(0,3);
-// });
-// console.log(op);
+// crud on obj 
+
+// read 
+// 1.dot notation
+console.log(students.name);  // students obj se mujhe name key ki value do
+// 2. bracket notation
+console.log(students['course']);
 
 
-// 5 covert all numbers into string with $ sign 
+
+// update 
+// same key ko new value do 
+
+students.age = 23;
+console.log(students);
 
 
-// const arr = [10,20,30];
-// let op = arr.map((el)=>{
-// return ` $${el}`;
-// })
-// console.log(op);
+// delete 
+// keys ko delte krna 
+
+delete students.course;
 
 
-// 6 take a sentence return an array of each word length
-// const str = "coding is fun";
+// obj methods 
 
-// let op = str.split(" ").map((el) => {
-//     return el.length;
-// });
+//object.keys - obj ke sare keys ko array mein daal deta h 
 
-// console.log(op);
+console.log(Object.keys(students));
 
-// 7 make every word in a str uppercase
+//Object.values - obj ki sari values ko array mein daal deta hain '
 
-// let str = 'hello bhiya';
-// let op = str.split(" ").map((el)=>{
-//     return el.toUpperCase();
-// })
-// console.log(op);
-
-// 8. replace all vowels with in a word with *
-
-// const arr = ["apple", "banana", "car"];
-
-// const op = arr.map((word) => {
-//     return word
-//         .split("")
-//         .map((char) => {
-//             return "aeiou".includes(char) ? "*" : char;
-//         })
-//         .join("");
-// });
-
-// console.log(op);
-
-// move all the zeros to the last
-// let arr = [0,1,0,2,3,0,3];
-// let zero = arr.filter(el => el === 0);
-// let nonzero = arr.filter(el => el !== 0);
-// let op1 = nonzero.concat(zero);
-// console.log(op1);
+console.log(Object.values(students));
 
 
-//from a sen filter out shorter then  4 letters then make it remaing words uppercase
+// Object.entries - obj ki keys+value dono ko array mein daal deta hai  
 
-// let str = 'i love you javscipt';
+console.log(Object.entries(students));
 
-// let op = str
-//     .split(' ')
-//     .filter((el) => {
-//         return el.length >= 4;
-//     })
-//     .map((el) => {
-//         return el.toUpperCase();
-//     });
+// for in loop 
 
-// console.log(op);
 
-// check if a name includes vowel or not return true and false
-// let name = 'pranav';
-// let op = name.split("").some((char)=>{
-//     return 'aeiou'.includes(char);
-// })
-// console.log(op);
+for(let key in students){
+    console.log(key);
+    
+}
 
-// filter the names starting with vowel
+// Object.seal() - Existing properties ko update kar sakte ho, lekin new property add/delete nahi kar sakte.
 
-// let names = ["atul","pranavii","anurag"];
-// let op  = names.filter((el)=>{
-//     return "aeiou".includes(el.charAt(0));
-// })
-// console.log(op);
+let user = {
+    name: "Aadarsh",
+    age: 22
+};
+
+Object.seal(user);
+
+user.age = 25;        // allowed
+user.city = "Raipur"; // not allowed
+delete user.name;     // not allowed
+
+
+// Object freeze - obj ko change krne nahi deta
+
+// if you create obj then give 
+// Object.freeze(user); // ikse baad kuch bhi nahi ho payega 
+
+
+
+// hasOwnProperty() obj mein specific property h ya nahi
+// console.log(user.hasOwnProperty(age));
+let obj1 = {
+    name : "addy"
+};
+let obj2 = {
+    // name : "addy"
+    age : 22
+};
+
+let op = Object.assign({},obj1,obj2);
+console.log(op);
+// modern way 
+let op2 = {
+    ...obj1,
+    ...obj2
+};
+
+
 
